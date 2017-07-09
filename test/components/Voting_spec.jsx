13b7b8cd5886767b9025ca08ca66286b45a7d8af
Fred Wong <fredwong.it@gmail.com>
2017-07-09 12:02:59 -0400

    Immutable Data And Pure Rendering

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -7,6 +7,7 @@ import {
 } from 'react-addons-test-utils';
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai';
+import {List} from 'immutable';
 
 describe('Voting', () => {
     it('renders a pair of buttons', () => {
@@ -65,4 +66,46 @@ describe('Voting', () => {
         expect(winner).to.be.ok;
         expect(winner.textContent).to.contain('apple');
     });
+
+    it('renders as a pure component', () => {
+        const pair = ['book', 'apple'];
+        const container = document.createElement('div');
+        let component = ReactDOM.render(
+            <Voting pair={pair} />,
+            container
+        );
+
+        let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+        expect(firstButton.textContent).to.equal('book');
+
+        pair[0] = 'hero';
+        component = ReactDOM.render(
+            <Voting pair={pair} />,
+            container
+        );
+
+        firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+        expect(firstButton.textContent).to.equal('book');
+    });
+
+    it('does update DOM when prop changes', () => {
+        const pair = List.of('book', 'apple');
+        const container = document.createElement('div');
+        let component = ReactDOM.render(
+            <Voting pair={pair} />,
+            container
+        );
+
+        let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+        expect(firstButton.textContent).to.equal('book');
+
+        const newPair = pair.set(0, 'hero')
+        component = ReactDOM.render(
+            <Voting pair={pair} />,
+            container
+        );
+
+        firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0];
+        expect(firstButton.textContent).to.equal('book');
+    })
 });
