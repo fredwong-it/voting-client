9c92e43ef34dc9737eebcb84d01904dbd22817ea
Fred Wong <fredwong.it@gmail.com>
2017-07-10 15:22:47 -0400

    Writing The UI for The Results Screen And Handling Routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
+import Results from './components/Results';
 
-const pair = ['book', 'apple'];
+
+const routes = <Route component={App}>
+    <Route path="/results" component={Results} />
+    <Route path="/" component={Voting} />  
+</Route>;
 
 ReactDOM.render(
     //<Voting pair={pair}  />,
-    <Voting pair={pair} hasVoted="book" />,
+    //<Voting pair={pair} hasVoted="book" />,
     //<Voting pair={pair} winner="apple" />,
+    <Router history={hashHistory}>{routes}</Router>,
     document.getElementById('app')
 );
